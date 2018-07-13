52e9968163a10a5a30cf2b4d02560e76832282e2
Eysenck Gómez <eysenck.gomez@adroll.com>
2018-07-13 18:50:38 -0500

    Add some tests.

--- test/currency_conversion_test.exs
+++ test/currency_conversion_test.exs
@@ -5,4 +5,14 @@ defmodule CurrencyConversionTest do
   test "checks if btc_to_usd returns float" do
     assert CurrencyConversion.btc_to_usd(100) |> is_float == true
   end
+
+  test "checks if usd_to_btc returns float" do
+    assert CurrencyConversion.btc_to_usd(100) |> is_float == true
+  end
+
+  test "checks if btc_to_usd returns inverse of usd_to_btc" do
+    btc = 10.0
+    usd = CurrencyConversion.btc_to_usd(btc)
+    assert btc == CurrencyConversion.usd_to_btc(usd)
+  end
 end
