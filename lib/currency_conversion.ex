f0ae16729a4faff0a1a9a52c436985b2e2a4a8b2
Eysenck Gómez <eysenck.gomez@adroll.com>
2018-07-13 19:14:40 -0500

    Add conversion of btc to rub and rub to btc. Add some tests.

--- lib/currency_conversion.ex
+++ lib/currency_conversion.ex
@@ -3,7 +3,7 @@ defmodule CurrencyConversion do
   Documentation for CurrencyConversion.
   """
 
-  defp get_btc_to_usd_rate() do
+  defp get_btc_to_usd_rate do
     HTTPoison.start
     case HTTPoison.get "https://api.pro.coinbase.com/products/BTC-USD/candles" do
       {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
@@ -12,6 +12,17 @@ defmodule CurrencyConversion do
         low = Enum.at(last_rate, 2) ## highest price during the bucket interval
         rate = (high + low) / 2
         Float.ceil rate
+      {:error, %HTTPoison.Error{reason: reason}} ->
+        IO.inspect reason
+    end
+  end
+
+  defp get_btc_to_rub_rate do
+    HTTPoison.start
+    case HTTPoison.get "https://api.coingecko.com/api/v3/exchange_rates" do
+      {:ok, %HTTPoison.Response{status_code: 200, body: body}} ->
+        json = Jason.decode!(body)
+        json["rates"]["rub"]["value"]
       {:error, %HTTPoison.Error{reason: reason}} ->
         IO.inspect reason
     end
@@ -32,6 +43,27 @@ defmodule CurrencyConversion do
   """
   def usd_to_btc(amount, precision \\ 2) do
     case get_btc_to_usd_rate() do
+      rate -> Float.ceil (1/rate) * amount, precision
+      _ -> nil
+    end
+  end
+
+
+  @doc """
+  Converts BTC to RUB using the CoinGecko Get Rates endpoint
+  """
+  def btc_to_rub(amount, precision \\ 2) do
+    case get_btc_to_rub_rate() do
+      rate -> Float.ceil rate * amount, precision
+      _ -> nil
+    end
+  end
+
+  @doc """
+  Converts RUbB to BTC using the CoinGecko Get Rates endpoint
+  """
+  def rub_to_btc(amount, precision \\ 2) do
+    case get_btc_to_rub_rate() do
       rate -> Float.ceil (1/rate) * amount, precision
       _ -> nil
     end
